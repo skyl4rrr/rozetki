b7ce3bf4e1eaa3c740aeb6bbd0ec232e9c36fab9
skyl4rrr <151707122+skyl4rrr@users.noreply.github.com>
2024-11-03 22:04:02 +0500

    Add files via upload

--- scripts/node.js
+++ scripts/node.js
@@ -1,14 +1,14 @@
 $(".whatis").click(function () {
-  $("html, body").animate({ scrollTop: 265 }, 800);
+  $("html, body").animate({ scrollTop: 245 }, 800);
 });
 $(".howuse").click(function () {
-  $("html, body").animate({ scrollTop: 935 }, 800);
+  $("html, body").animate({ scrollTop: 1065 }, 800);
 });
 $(".in").click(function () {
-  $("html, body").animate({ scrollTop: 1640 }, 800);
+  $("html, body").animate({ scrollTop: 1767 }, 800);
 });
 $(".history").click(function () {
-  $("html, body").animate({ scrollTop: 2150 }, 800);
+  $("html, body").animate({ scrollTop: 2215 }, 800);
 });
 $(".corpus").click(function () {
   $(".in-btn-p").text(
